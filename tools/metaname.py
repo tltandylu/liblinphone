tltandylu/liblinphone

commit 0c99e7fa767e268741cb6e602768dffcde1d53bc

Use a specific name translator for Javadoc references

tools/metaname.py
@@ -233,7 +233,7 @@ class NamespaceName(Name):
 		return translator.translate_namespace_name(self, **params)
 
 
-class Translator:
+class Translator(object):
 	instances = {}
 	
 	@staticmethod
@@ -280,6 +280,7 @@ class CTranslator(Translator):
 class JavaTranslator(Translator):
 	def __init__(self):
 		self.nsSep = '.'
+		self.classMemberSep = '.'
 		self.keyWordEscapes = {}
 		self.lowerMethodNames = True
 		self.lowerNamespaceNames = True
@@ -289,7 +290,7 @@ class JavaTranslator(Translator):
 			return name.to_camel_case()
 		else:
 			params = {'recursive': recursive, 'topAncestor': topAncestor}
-			return name.prev.translate(self, **params) + self.nsSep + name.to_camel_case()
+			return name.prev.translate(self, **params) + self._get_separator(name.prev) + name.to_camel_case()
 	
 	def translate_interface_name(self, name, **params):
 		return self.translate_class_name(name, **params)
@@ -308,7 +309,7 @@ class JavaTranslator(Translator):
 			return translatedName
 		else:
 			params = {'recursive': recursive, 'topAncestor': topAncestor}
-			return name.prev.translate(self, **params) + self.nsSep + translatedName
+			return name.prev.translate(self, **params) + self._get_separator(name.prev) + translatedName
 	
 	def translate_namespace_name(self, name, recursive=False, topAncestor=None):
 		translatedName = name.concatenate() if self.lowerNamespaceNames else name.to_camel_case()
@@ -316,7 +317,7 @@ class JavaTranslator(Translator):
 			return translatedName
 		else:
 			params = {'recursive': recursive, 'topAncestor': topAncestor}
-			return name.prev.translate(self, **params) + self.nsSep + translatedName
+			return name.prev.translate(self, **params) + self._get_separator(name.prev) + translatedName
 
 	def translate_argument_name(self, name):
 		argname = name.to_camel_case(lower=True)
@@ -330,6 +331,15 @@ class JavaTranslator(Translator):
 			return self.keyWordEscapes[keyword]
 		except KeyError:
 			return keyword
+
+	def _get_separator(self, prefix):
+		if prefix is None:
+			return ''
+		elif type(prefix) in (ClassName, EnumName):
+			return self.classMemberSep
+		else:
+			return self.nsSep
+
 
 class SwiftTranslator(JavaTranslator):
 	def __init__(self):

tools/metadoc.py
@@ -409,12 +409,13 @@ class ReferenceTranslationError(TranslationError):
 		return '{0} reference could not been translated'.format(self.args[0])
 
 
-class Translator:
+class Translator(object):
 	def __init__(self, langCode):
 		self.langCode = langCode
 		self.textWidth = 80
 		self.nameTranslator = metaname.Translator.get(langCode)
 		self.langTranslator = abstractapi.Translator.get(langCode)
+		self.refNameTranslator = self.nameTranslator
 		self.displaySelfParam = True if langCode == 'C' else False
 	
 	def translate_description(self, description, tagAsBrief=False):
@@ -456,7 +457,7 @@ class Translator:
 				commonName = namespace.name
 			else:
 				commonName = metaname.Name.find_common_parent(ref.relatedObject.name, namespace.name)
-		return ref.relatedObject.name.translate(self.nameTranslator, recursive=True, topAncestor=commonName)
+		return ref.relatedObject.name.translate(self.refNameTranslator, recursive=True, topAncestor=commonName)
 	
 	def translate_keyword(self, keyword):
 		return keyword.keyword.translate(self.langTranslator)
@@ -535,10 +536,10 @@ class DoxygenTranslator(Translator):
 			lines[0] = '@brief ' + lines[0]
 
 	def translate_class_reference(self, ref, **kargs):
-		return '@ref ' + Translator.translate_reference(self, ref)
+		return '@ref ' + super(DoxygenTranslator, self).translate_reference(ref)
 
 	def translate_function_reference(self, ref, **kargs):
-		return Translator.translate_reference(self, ref) + '()'
+		return super(DoxygenTranslator, self).translate_reference(ref) + '()'
 	
 	def _translate_section(self, section):
 		return '@{0} {1}'.format(
@@ -556,20 +557,22 @@ class DoxygenTranslator(Translator):
 		return text
 
 
-class JavaDocTranslator(DoxygenTranslator):
-	def __init__(self):
-		DoxygenTranslator.__init__(self, 'Java')
+class JavaDocTranslator(Translator):
+	class ReferenceTranslator(metaname.JavaTranslator):
+		def __init__(self):
+			super(JavaDocTranslator.ReferenceTranslator, self).__init__()
+			self.classMemberSep = '#'
 
-	def _tag_as_brief(self, lines):
-		pass
+
+	def __init__(self):
+		super(JavaDocTranslator, self).__init__('Java')
+		self.refNameTranslator = JavaDocTranslator.ReferenceTranslator()
 
 	def translate_class_reference(self, ref, **kargs):
-		return '{@link ' + Translator.translate_reference(self, ref) + '}'
+		return '{@link ' + super(JavaDocTranslator, self).translate_reference(ref) + '}'
 
 	def translate_function_reference(self, ref, **kargs):
-		className = ref.relatedObject.name.prev.translate(self.nameTranslator)
-		methodName = ref.relatedObject.name.translate(self.nameTranslator)
-		return '{@link ' + className + '#' + methodName + '}'
+		return '{@link ' + super(JavaDocTranslator, self).translate_reference(ref) + '}'
 	
 	def _translate_section(self, section):
 		if section.kind == 'see':
@@ -594,12 +597,15 @@ class JavaDocTranslator(DoxygenTranslator):
 		return text
 
 
-class SwiftDocTranslator(JavaDocTranslator):
+class SwiftDocTranslator(Translator):
 	def __init__(self):
-		DoxygenTranslator.__init__(self, 'Swift')
+		super(JavaDocTranslator, self).__init__('Swift')
 
 	def translate_class_reference(self, ref, **kargs):
-		return '`{0}`'.format(Translator.translate_reference(self, ref))
+		return '`{0}`'.format(super(JavaDocTranslator, self).translate_reference(ref))
+
+	def translate_function_reference(self, ref, **kargs):
+		return super(JavaDocTranslator, self).translate_reference(ref) + '()'
 
 	def _translate_section(self, section):
 		if section.kind == 'return':

commit c5562b788849cd17e3731ab2d1a3e4000a0025b8

Fix regression on wrapper generator caused by 0c99e7fa767e268741cb6e602768dffcde1d53bc

tools/metaname.py
@@ -280,10 +280,18 @@ class CTranslator(Translator):
 class JavaTranslator(Translator):
 	def __init__(self):
 		self.nsSep = '.'
-		self.classMemberSep = '.'
+		self._classMemberSep = None # None meams equal to self.nsSep
 		self.keyWordEscapes = {}
 		self.lowerMethodNames = True
 		self.lowerNamespaceNames = True
+
+	@property
+	def classMemberSep(self):
+		return self._classMemberSep if self._classMemberSep is not None else self.nsSep;
+
+	@classMemberSep.setter
+	def classMemberSep(self, sep):
+		self._classMemberSep = sep
 	
 	def translate_class_name(self, name, recursive=False, topAncestor=None):
 		if name.prev is None or not recursive or name.prev is topAncestor:
@@ -360,6 +368,7 @@ class SwiftTranslator(JavaTranslator):
 
 	def translate_class_name(self, name, recursive=False, topAncestor=None):
 		return name.to_camel_case()
+
 
 class CppTranslator(JavaTranslator):
 	def __init__(self):